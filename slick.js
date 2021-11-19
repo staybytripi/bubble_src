50d181b953f2b7f16ab2fb148d44a2bfe7417601
staybytripi <83488960+staybytripi@users.noreply.github.com>
2021-11-19 23:19:10 +0900

    Update slick.js

--- slick.js
+++ slick.js
@@ -1,7 +1,7 @@
 	$('.slider').slick({
 		autoplay: false,//自動的に動き出すか。初期値はfalse。
 		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		slidesToShow: 2.5,//スライドを画面に3枚見せる
+		slidesToShow: 2,//スライドを画面に3枚見せる
 		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
 		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
 		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
